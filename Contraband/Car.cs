d51953fff3308eef2368e458a3f93d825e0f34fe
Harlintong Urbina Bermudez <harlintonggeonieth@learnet.se>
2020-01-28 13:24:15 +0100

    couldn't try if examine works

---
+++ Contraband/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contraband
+{
+    class Car
+    {
+        public Random generator = new Random();
+
+        public int passenger;
+        public int contrabandAmount;
+        public bool alredyChecked;
+
+        public bool Examine()
+        {
+            alredyChecked = true;
+
+            if (contrabandAmount <= 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+                
+            }
+        }
+    }
+}
