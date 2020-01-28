e41c25a2380f664f5b6d559f37a0e50cc77f26f9
Harlintong Urbina Bermudez <harlintonggeonieth@learnet.se>
2020-01-28 13:24:25 +0100

    WIP

---
+++ Contraband/ContrabandCar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contraband
+{
+    class ContrabandCar : Car
+    {
+        public ContrabandCar()
+        {
+        }
+    }
+}
